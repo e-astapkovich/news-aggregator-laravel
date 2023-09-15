5731d281c93f4a974e1a7503671038ec5d3f7565
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-09-15 21:43:52 +0500

    Добавил тесты

---
+++ tests/Feature/http/RoutingTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class RoutingTest extends TestCase {
+
+    public function test_news_page_returns_a_successful_response() {
+        $response = $this->get('/news');
+        $response->assertStatus(200);
+    }
+
+    public function test_news_categories_page_returns_a_successful_response() {
+        $response = $this->get('/news-categories');
+        $response->assertStatus(200);
+    }
+
+    public function test_admin_page_returns_a_successful_response() {
+        $response = $this->get('/admin');
+        $response->assertStatus(200);
+    }
+
+}
