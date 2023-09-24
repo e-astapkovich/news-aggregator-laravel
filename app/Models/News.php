2a0e3badbb963a6b52243a558a690a170af7b6ab
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-09-24 10:08:22 +0500

    Добавил отношение один ко многим между моделями Categories и News

--- app/Models/News.php
+++ app/Models/News.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class News extends Model
 {
@@ -17,4 +18,8 @@ class News extends Model
         'image',
         'status'
     ];
+
+    public function category(): BelongsTo {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
 }
