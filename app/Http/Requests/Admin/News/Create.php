f478b480ce8baac87e867f11552553ce8741bdc3
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-09-26 00:35:25 +0500

    Добавил валидацию через Request для формы добавления и редактирования новостей

---
+++ app/Http/Requests/Admin/News/Create.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Admin\News;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Category;
+use Illuminate\Validation\Rules\Enum;
+use App\Enums\News\Status;
+
+class Create extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        $tableNameCategory = (new Category())->getTable();
+        return [
+            'title' => ['required', 'string', 'min:5', 'max: 100'],
+            'description' => ['nullable', 'string'],
+            'category_id' => ['required', 'integer', "exists:{$tableNameCategory},id"],
+            'author' => ['required', 'string', 'min:2', 'max:100'],
+            'image' => ['nullable', 'image'],
+            'status' => ['required', new Enum(Status::class)]
+        ];
+    }
+}
--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -8,6 +8,12 @@
     <h1 class="h2">Добавить новость</h1>
 </div>
 
+{{-- @if ($errors->any())
+    @foreach ($errors->all() as $error)
+        <x-alert :message="$error" type='danger'></x-alert>
+    @endforeach
+@endif --}}
+
 @include('inc.message')
 
 <form method="post" action="{{ route('admin.news.store') }}">
@@ -15,40 +21,51 @@
     @csrf
     <div class="form-group">
         <label for="title">Заголовок</label>
-        <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}">
+        <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title" value="{{ old('title') }}">
+        @error('title')
+            <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
     </div>
     <div class="form-group">
         <label for="author">Автор</label>
-        <input type="text" class="form-control" name="author" id="author" value="{{ old('author') }}">
+        <input type="text" class="form-control @error('author') is-invalid @enderror" name="author" id="author" value="{{ old('author') }}">
+        @error('author')
+            <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
     </div>
     <div class="form-group">
         <label for="description">Описание</label>
-        <textarea class="form-control" name="description" id="description">{{ old('description') }}</textarea>
+        <textarea class="form-control" name="description @error('description') is-invalid @enderror" id="description">{{ old('description') }}</textarea>
+        @error('description')
+            <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
     </div>
     <div class="form-group">
         <label for="category">Автор</label>
-        <select class="form-select" name="category_id" id="category">
-
+        <select class="form-select @error('category') is-invalid @enderror" name="category_id" id="category">
             @foreach ($categories as $category)
-
                 <option value="{{ $category->id }}" @selected($category->id == old('category_id'))>
                     {{ $category->name }}
                 </option>
-
             @endforeach
-
         </select>
+        @error('category')
+            <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
     </div>
     <div class="form-group">
         <label for="status">Статус</label>
-        <select class="form-select" name="status" id="status">
+        <select class="form-select @error('status') is-invalid @enderror" name="status" id="status">
             <option @if(old('status')==='draft' ) selected @endif>draft</option>
             <option @if(old('status')==='active' ) selected @endif>active</option>
             <option @if(old('status')==='blocked' ) selected @endif>blocked</option>
         </select>
+        @error('status')
+            <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
     </div>
     <br>
-    <button type="submit" class="btn btn-success">Save</button>
+    <button type="submit" class="btn btn-success">Добавить новость</button>
 </form>
 
 @endsection
