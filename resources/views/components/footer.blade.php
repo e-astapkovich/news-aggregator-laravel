369298743e3e5cde74d016a827acc3e9cac8fd0b
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-10-03 22:50:13 +0500

    изменил цвета футера

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,10 @@
-<footer class="text-body-secondary py-5 layout__footer">
-    <div class="container">
-        <p class="float-end mb-1">
-            <a href="#">Back to top</a>
-        </p>
-        <p class="mb-1">Template example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-    </div>
-</footer>
+<div class="container">
+    <footer class="text-white bg-dark bg-gradient py-4 layout__footer">
+        <div class="container">
+            <p class="float-end mb-1">
+                <a href="#" class="text-white">Back to top</a>
+            </p>
+            <p class="mb-1">Template example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+        </div>
+    </footer>
+</div>
