7df585bb1866a4108e7ac324dd054d3233d88028
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-10-03 22:54:58 +0500

    поправил title админки

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.admin')
+
+@section('title') Категории @parent @stop
+
 @section('content')
 
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
