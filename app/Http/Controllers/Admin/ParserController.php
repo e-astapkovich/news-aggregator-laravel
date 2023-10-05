67927d2cbb7b7dce62613fd6eb3f214cf502e13a
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-10-05 23:38:08 +0500

    Добавили контроллер парсера

---
+++ app/Http/Controllers/Admin/ParserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Orchestra\Parser\Xml\Facade as XmlParser;
+
+class ParserController extends Controller
+{
+    //
+}
