c8531eedb40462e7b540beb4c338a193c9c490b6
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-10-05 23:16:51 +0500

    доделали функционал логина через ВК

--- app/Http/Controllers/SocialProvidersController.php
+++ app/Http/Controllers/SocialProvidersController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialProvidersController extends Controller
@@ -12,7 +16,32 @@ class SocialProvidersController extends Controller
     }
 
     public function callback() {
-        $user = Socialite::driver('vkontakte')->user();
-        dd($user);
+
+        try {
+            $socialUser = Socialite::driver('vkontakte')->user();
+        } catch (Exception $error) {
+            return redirect('/login');
+        }
+
+        /*
+        * Если пользователь с данным email есть в БД, то авторизуем его. Если в БД нет, то создаем.
+        */
+
+        $user = User::where('email', $socialUser->getEmail())->first();
+
+        if (!$user) {
+            $user = new User([
+                'name' => $socialUser->name,
+                'email' => $socialUser->email,
+                'avatar' => $socialUser->avatar,
+                'password' => Hash::make('123')
+            ]);
+        }
+
+        $user->avatar = $socialUser->avatar;
+        $user->save();
+        Auth::login($user, true);
+
+        return redirect(route('welcome'));
     }
 }
