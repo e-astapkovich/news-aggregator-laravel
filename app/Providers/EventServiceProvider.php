6a9009eb31132c64554ba4478211f29f948d24b6
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-10-07 23:58:14 +0500

    Назначили слушателю событие

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\DefineLoginEvent;
+use App\Listeners\LastLoginUpdateListener;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -21,6 +23,9 @@ class EventServiceProvider extends ServiceProvider
         \SocialiteProviders\Manager\SocialiteWasCalled::class => [
             \SocialiteProviders\VKontakte\VKontakteExtendSocialite::class.'@handle',
         ],
+        DefineLoginEvent::class => [
+            LastLoginUpdateListener::class
+        ]
     ];
 
     /**
