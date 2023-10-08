14b9b050f2910964c00547187567d973043fccc1
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-10-08 18:53:36 +0500

    Перенесли логику из ParserController в сервис

---
+++ app/Services/ParserService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Service;
+
+use App\Services\Interfaces\Parser;
+use Orchestra\Parser\Xml\Facade as XmlParser;
+
+class ParserService implements Parser
+{
+    private string $link;
+    public function setLink(string $url): Parser
+    {
+        $this->link = $url;
+        return $this;
+    }
+
+    public function saveParseData(): void
+    {
+        $parser = XMLParser::load($this->link);
+
+        $news = $parser->parse([
+            'title' => ['uses' => 'channel.title'],
+            'link' => ['uses' => 'channel.link'],
+            'description' => ['uses' => 'channel.description'],
+            'image' => ['uses' => 'channel.image.url'],
+            'news' => ['uses' => 'channel.item[title,description,author,pubDate,category,enclosure::url]'],
+        ]);
+
+        dd($news);
+    }
+}
