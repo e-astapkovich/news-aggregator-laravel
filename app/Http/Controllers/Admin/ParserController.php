706343f587310c8dd5498ebe3604067379af5cc0
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-10-08 20:18:14 +0500

    Забиндил контракт Parser и класс ParserService в AppServiceProvider

--- app/Http/Controllers/Admin/ParserController.php
+++ app/Http/Controllers/Admin/ParserController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Services\ParserService;
+// use App\Services\ParserService;
+use App\Services\Interfaces\Parser;
 use Illuminate\Http\Request;
-use Orchestra\Parser\Xml\Facade as XmlParser;
+// use Orchestra\Parser\Xml\Facade as XmlParser;
 
 class ParserController extends Controller
 {
-    public function __invoke(Request $request, ParserService $parserService)
+    public function __invoke(Request $request, Parser $parserService)
     {
         $url = "https://lenta.ru/rss";
         $url2 = "https://news.rambler.ru/rss/tech/";
