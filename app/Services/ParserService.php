3e1a1b05c67cf34a0ae1a8a263133c82618fed92
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-10-10 01:16:07 +0500

    в сервис парсера добавил логику сохранения новостей в БД

--- app/Services/ParserService.php
+++ app/Services/ParserService.php
@@ -5,6 +5,8 @@ namespace App\Services;
 use App\Services\Interfaces\IParser;
 use Orchestra\Parser\Xml\Facade as XmlParser;
 use App\Models\Category;
+use App\Models\News;
+use App\Enums\News\Status;
 
 class ParserService implements IParser
 {
@@ -32,18 +34,32 @@ class ParserService implements IParser
         // Из спарсенного списка новостей извлекаем названия категорий, и если его нет в БД (таблица Categories), то добавляем
         $this->checkAndSaveCategories();
 
+        foreach ($this->parsedData['newsList'] as $newsItem) {
+
+            News::firstOrCreate(
+                [
+                    'title' => $newsItem['title'],
+                ],
+                [
+                    'description' => $newsItem['description'],
+                    'category_id' => Category::firstWhere('name', $newsItem['category'])['id'],
+                    'author' => $newsItem['author'],
+                    'image' => $newsItem['enclosure::url'] ?? $this->parsedData['image'],
+                    'status' => Status::ACTIVE->value,
+                    'created_at' => now(),                           // дата создания ЗАПИСИ в БД
+                ]
+            );
+        }
+
         dd($this->parsedData);
     }
 
     protected function checkAndSaveCategories(): void
     {
-        foreach ($this->parsedData['newsList'] as $newsItem)
-        {
+        foreach ($this->parsedData['newsList'] as $newsItem) {
             Category::firstOrCreate(
                 ['name' => $newsItem['category']]
             );
         }
     }
-
-
 }
