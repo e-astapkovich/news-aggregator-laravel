da34f7e87b6ebfc977433d3b8012fc264a6fcfa9
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-10-09 23:41:55 +0500

    в сервис парсинга добавил метод, который из спарсенного списка извлекает названия категорий, проверяет, есть ли они в БД, и, если нет, то добавляет

--- app/Services/ParserService.php
+++ app/Services/ParserService.php
@@ -4,10 +4,12 @@ namespace App\Services;
 
 use App\Services\Interfaces\IParser;
 use Orchestra\Parser\Xml\Facade as XmlParser;
+use App\Models\Category;
 
 class ParserService implements IParser
 {
     private string $link;
+    private array $parsedData;
 
     public function setLink(string $url): IParser
     {
@@ -19,14 +21,29 @@ class ParserService implements IParser
     {
         $parser = XMLParser::load($this->link);
 
-        $news = $parser->parse([
+        $this->parsedData = $parser->parse([
             'title' => ['uses' => 'channel.title'],
             'link' => ['uses' => 'channel.link'],
             'description' => ['uses' => 'channel.description'],
             'image' => ['uses' => 'channel.image.url'],
-            'news' => ['uses' => 'channel.item[title,description,author,pubDate,category,enclosure::url]'],
+            'newsList' => ['uses' => 'channel.item[title,description,author,pubDate,category,enclosure::url]'],
         ]);
 
-        dd($news);
+        // Из спарсенного списка новостей извлекаем названия категорий, и если его нет в БД (таблица Categories), то добавляем
+        $this->checkAndSaveCategories();
+
+        dd($this->parsedData);
     }
+
+    protected function checkAndSaveCategories(): void
+    {
+        foreach ($this->parsedData['newsList'] as $newsItem)
+        {
+            Category::firstOrCreate(
+                ['name' => $newsItem['category']]
+            );
+        }
+    }
+
+
 }
