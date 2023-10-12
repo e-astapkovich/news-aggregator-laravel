c15d538a7009858fab6dd7ada7e82b0031dc50b2
Евгений Астапкович <ni-shagu-nazad@yandex.ru>
2023-10-13 01:30:11 +0500

    Добавил модель для работы с таблицей ресурсов для парсинга. Переписал контроллер парсера на работу через модель

--- app/Http/Controllers/Admin/ParserController.php
+++ app/Http/Controllers/Admin/ParserController.php
@@ -3,25 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-// use App\Services\ParserService;
+use App\Models\ParsingResource;
 use App\Services\Interfaces\IParser;
 use Illuminate\Http\Request;
-// use Orchestra\Parser\Xml\Facade as XmlParser;
 
 class ParserController extends Controller
 {
-    private static array $newsSources = [
-        "https://news.rambler.ru/rss/tech/",
-        "https://news.rambler.ru/rss/games/",
-        "https://news.rambler.ru/rss/starlife/",
-        "https://lenta.ru/rss",
-    ];
-
     public function __invoke(Request $request, IParser $parserService)
     {
-        foreach (self::$newsSources as $url)
+        $parsingResources = ParsingResource::get(['url']);
+
+        foreach ($parsingResources as $resource)
         {
-            $parserService->setLink($url)->saveParseData();
+            $parserService->setLink($resource->url)->saveParseData();
         }
         return redirect()->route('admin.news.index')->with('success', "Новости загружены");
     }
